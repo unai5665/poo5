af7857ef489859d191131c68b7cbfaad55492a0e
unai5665 <unaiperez5665@gmail.com>
2024-05-15 09:33:23 +0200

    creada clase tarea

---
+++ hgt/tarea.py
@@ -0,0 +1,9 @@
+class Tarea:
+    def __init__(self, tarea:str, estado:bool, id:str) -> None:
+        self.tarea  = tarea
+        self.id     = id
+        self.estado = estado
+
+    
+    def create(self)
+        
