347faa55948399cb7bdaed190ac3fd34ec485eee
unai5665 <unaiperez5665@gmail.com>
2024-05-21 12:56:28 +0200

    terminada clase tarea

--- hgt/tarea.py
+++ hgt/tarea.py
@@ -1,18 +1,27 @@
 class Tarea:
-    def __init__(self, tarea:str, estado:bool, id:str) -> None:
+    def __init__(self, tarea:str, estado:bool, id:int) -> None:
         self.tarea  = tarea
         self.id     = id
         self.estado = estado
 
     #Método CRUD
-    def delete(self):
-       self.estado = False
-
+    def read(self)->str:
+        return f"{self.tarea}, {self.estado}, {self.id}"
     
-    def update(self, nueva_tarea:str):
+
+    def update(self, nueva_tarea:str, nuevo_estado:bool, nuevo_id:int)->None:
         self.tarea = nueva_tarea
+        self.id = nuevo_id
+        self.estado = nuevo_estado 
+
+    def delete(self):
+       self.estado = None
+       self.id = None
+       self.estado = None
 
     
-    def read(self):
-        return self.tarea
+    
+
+    
+    
         
